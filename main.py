98f05103b1418d913dbf96956038081b2bc9d450
Joao Carlos Alves <suporte_carlos@live.com>
2022-05-23 11:36:45 -0300

    estragando a logica

--- main.py
+++ main.py
@@ -10,29 +10,42 @@
 aprovados=0
 exame=0
 reprovados=0
-#sexo=""
+genero=''
+
 cadastrar = input('Deseja iniciar lançamentos (S/N)? ' )
+
 while cadastrar.upper() !='S' and cadastrar.upper()!='N':
+
     cadastrar= input('Opção Inválida, informe (S/N): ')
 
 while cadastrar.upper() == 'S':
-    nome = input('Informe nome do aluno: ')
-    sexo = input('Informe o sexo do aluno (M ou F):')
+    
+    # nome = input('Informe nome do aluno: ')
+    
+    # genero = input('Informe o Genero do aluno(M/F): ')
+
+    # while genero.upper() !='M' and genero.upper()!='F':
+
+    #     genero= input('Opção Inválida, informe (M/F): ')
+    
+    #while genero.upper() == 'M' or genero.upper() == 'F':
+
     for cont in range (1,4):
-        if cont==1:
-            nota1 = int(input('Informe N1:'))
-            while nota1<0 or nota1>10:
-                nota1 =int(input('N1 inválido, valor deve ser entre 0 e 10: '))
-        elif cont==2:
-            nota2 = int(input('Informe N2:'))
-            while nota2 < 0 or nota2 > 10:
-                nota2 = int(input('N2 inválido, valor deve ser entre 0 e 10: '))
-        else:
-            nota3 = int(input('Informe N3:'))
-            while nota3 < 0 or nota3 > 10:
-                nota3 = int(input('N3 inválido, valor deve ser entre 0 e 10: '))
+       if cont==1:
+           nota1 = int(input('Informe N1:'))
+           while nota1<0 or nota1>10:
+               nota1 =int(input('N1 inválido, valor deve ser entre 0 e 10: '))
+       elif cont==2:
+           nota2 = int(input('Informe N2:'))
+       while nota2 < 0 or nota2 > 10:
+           nota2 = int(input('N2 inválido, valor deve ser entre 0 e 10: '))
+    else:
+       nota3 = int(input('Informe N3:'))
+       while nota3 < 0 or nota3 > 10:
+           nota3 = int(input('N3 inválido, valor deve ser entre 0 e 10: '))
 
     #calcular média e verificar status do alunos
+    
     media = (nota1+nota2+nota3)/3
 
     print(str(media))
@@ -44,12 +57,7 @@ while cadastrar.upper() == 'S':
          exame+=1
     else:
          reprovados+=1
-
-    # #calcula numero alunos aprovados e reprovados
     
-
-
-
     #calcula media aluno feminino
     cadastrar = input('Deseja efetuar outro lançamento (S/N)? ')
     while cadastrar.upper() != 'S' and cadastrar.upper() != 'N':
@@ -62,35 +70,28 @@ print('Total de alunos APROVADOS: ' +str(aprovados))
 print('Total de alunos de EXAME: ' +str(exame))
 print('Total de alunos REPROVADOS: ' +str(reprovados))
 
-if sexo=='M' and aprovados:
+if genero=='M' and aprovados:
 
     print('Total de pessoas do sexo masculino aprovadas'+ str(aprovados))
 
-if sexo=='F' and aprovados:
+if genero=='F' and aprovados:
 
     print('Total de pessoas do sexo feminino aprovadas'+ str(aprovados))
 
-if sexo=='M' and exame:
+if genero=='M' and exame:
 
     print('Total de pessoas do sexo masculino de exame'+ str(exame))
 
-if sexo=='F' and exame:
+if genero=='F' and exame:
 
     print('Total de pessoas do sexo feminino de exame'+ str(exame))
 
-if sexo=='M' and reprovados:
+if genero=='M' and reprovados:
 
     print('Total de pessoas do sexo masculino reprovadas'+ str(reprovados))
 
-if sexo=='F' and reprovados:
+if genero=='F' and reprovados:
 
     print('Total de pessoas do sexo feminino reprovadas'+ str(reprovados))
 
 
-#VALORES ABSOLUTOS
-# print('Total de pessoas do sexo feminino aprovadas' +str(feminino))
-# print('Total de pessoas do sexo masculino aprovadas' +str(masculino))
-# print('Total de pessoas do sexo feminino de exame' +str(femininoF))
-# print('Total de pessoas do sexo masculino de exame' +str(masculinoE))
-# print('Total de pessoas do sexo feminino reprovadas')
-# print('Total de pessoas do sexo masculino reprovadas')
